be4d3d3944e67680c7589309c48317571b060b77
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-04-16 23:57:18 +0530

    admin dashboard updated

--- routes/admin_bro.js
+++ routes/admin_bro.js
@@ -1,13 +1,15 @@
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
 const AdminBroMongoose = require('@admin-bro/mongoose')
+const TripModel = require('../models/trips')
 const UserModel = require('../models/users')
+const DriverModel = require('../models/drivers')
 
 
 AdminBro.registerAdapter(AdminBroMongoose)
 
 const AdminBroOptions = {
-    resources: [UserModel],
+    resources: [UserModel,DriverModel,TripModel]
 }
 
 console.log("Inside admin bro")
