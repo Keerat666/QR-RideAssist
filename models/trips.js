c42a1f6829381b9069c2e12fc39f543878738ffe
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-04-17 02:29:34 +0530

    otp and trip cost is auto generated randomly

--- models/trips.js
+++ models/trips.js
@@ -13,12 +13,15 @@ var tripSchema = new Schema({
     driver_id: { type: String, required: true, description: "Should be required" },
     user_details:{ type: {} , description: "Should be required" },
     driver_details:{ type: {} , description: "Should be required" },
-    otp: { type: Number, required: true, description: "Should be required" },
-    trip_fare: { type: Number, required: true, description: "Should be required" },
+    otp: { type: Number, required: true, description: "Should be required" , default : getRndInteger(1000,9999)},
+    trip_fare: { type: Number, required: true, description: "Should be required", default : getRndInteger(40,500) },
     trip_status: { type: String, enum: ['Active', 'Finished', 'Cancelled'], default : "Active" },
 
 }, { collection: 'trips' });
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
 var TripModel = mongoose.model('Trips', tripSchema);
 
